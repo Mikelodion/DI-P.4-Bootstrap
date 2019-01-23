0ecc1baec790722adf954f1ca3d4d71a546f6c3c
Miquel Janer Mudoy <miqueljanermudoy@outlook.com>
2019-01-23 12:14:18 +0100

    home,index,product

---
+++ js/js.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    document.getElementById("jumparrow").addEventListener('click', () => window.scrollTo({
+        top: 750,
+        behavior: 'smooth',
+      }));
+    
+}
