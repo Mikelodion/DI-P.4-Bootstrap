824ecc6b1cc5f8e18b1147b76408308f02594cc7
Miquel <miqueljanermudoy@outlook.com>
2019-02-01 17:40:59 +0100

    Detail Changes

--- js/js.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    document.getElementById("jumparrow").addEventListener('click', () => window.scrollTo({
-        top: 750,
-        behavior: 'smooth',
-      }));
-    
-}
